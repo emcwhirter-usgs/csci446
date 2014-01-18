8bfcd0741dd10f0c1851dbfcdd598d044e836414
Eddie McWhirter <emcwhirter@usgs.gov>
2014-01-18 16:06:10 -0700

    First attempt at level 3

--- src/unit2/rubywarrior/eddie-beginner/player.rb
+++ src/unit2/rubywarrior/eddie-beginner/player.rb
@@ -1,7 +1,14 @@
 class Player
   def play_turn(warrior)
     # add your code here
-    if warrior.feel.empty?
+    max_hp = 20
+    sludge_hp = 12
+    heal = 0.10
+
+    # If I'm missing 2 times the health I can heal in 1 turn.
+    if warrior.health < (1-2*heal)*max_hp
+        warrior.rest!
+    elsif warrior.feel.empty?
         warrior.walk!
     else
         warrior.attack!
