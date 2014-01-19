24932c9df054399a064890c4d7e22422786b3e7e
Eddie McWhirter <emcwhirter@usgs.gov>
2014-01-19 12:56:02 -0700

    Completed 4th Level

--- src/unit2/rubywarrior/eddie-beginner/player.rb
+++ src/unit2/rubywarrior/eddie-beginner/player.rb
@@ -1,14 +1,11 @@
 require 'warriorstats'
 
-
-
 class Player
 
   def play_turn(warrior)
     # add your code here
-    @@max_hp = 20
-    @@heal = 0.10
-    @@health = @@max_hp
+    @max_hp = 20
+    #heal = 0.10
     #sludge_max_hp = 12
     #thick_sludge_max_hp = 20
     #archer_max_hp = 7
@@ -16,12 +13,8 @@ class Player
     space_clear = warrior.feel.empty?
 
     if space_clear # Space I want to move to is empty
-      if warrior.health < @@max_hp # I'm hurt
-        if warrior.health >= @@health # not under attack
+      if !full_hp?(warrior) and !under_attack?(warrior) # I'm hurt & not under attack
           warrior.rest!
-        else # I'm under attack
-          warrior.walk!
-        end
       else # I'm full health
         warrior.walk!
       end
@@ -32,16 +25,16 @@ class Player
     @health = warrior.health
   end
 
-  def under_attack(current_health)
-        if current_health.to_i < @health.to_i
+  def under_attack?(warrior)
+        if warrior.health < @health
             result = true
         else
             result = false
         end
   end
 
-  def full_hp (current_health)
-      if current_health.to_i >= @max_hp.to_i
+  def full_hp?(warrior)
+      if warrior.health >= @max_hp
           result = true
       else
           result = false
