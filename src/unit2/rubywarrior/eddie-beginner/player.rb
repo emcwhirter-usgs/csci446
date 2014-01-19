5ce5f3da28682012e61ebe001579ff342efa0738
Eddie McWhirter <emcwhirter@usgs.gov>
2014-01-18 20:43:37 -0700

    Second attempt at level 4

--- src/unit2/rubywarrior/eddie-beginner/player.rb
+++ src/unit2/rubywarrior/eddie-beginner/player.rb
@@ -1,18 +1,32 @@
 require 'warriorstats'
 
+
+
 class Player
 
   def play_turn(warrior)
     # add your code here
-    sludge_max_hp = 12
-    thick_sludge_max_hp = 20
-    archer_max_hp = 7
+    @@max_hp = 20
+    @@heal = 0.10
+    @@health = @@max_hp
+    #sludge_max_hp = 12
+    #thick_sludge_max_hp = 20
+    #archer_max_hp = 7
 
-    me = WarriorStats.new
-    me.health_begin warrior.health
+    #me = WarriorStats.new
+    space_clear = warrior.feel.empty?
 
-    if warrior.feel.empty?
-        if not me.full_hp and not me.under_attack
+    #if under_attack(warrior.health)
+    if warrior.health < @@health # under attack
+      if warrior.health >= (1-8*@@heal)*@@max_hp and not space_clear # > 20% hp
+        warrior.attack!
+      else
+        warrior.rest!
+      end
+    elsif space_clear
+        #if not me.under_attack(warrior.health) and not me.full_hp(warrior.health)
+        #if not under_attack(warrior.health) and not full_hp(warrior.health)
+        if warrior.health < @@health and warrior.health < @@max_hp
             warrior.rest!
         else
             warrior.walk!
@@ -21,8 +35,25 @@ class Player
         warrior.attack!
     end
 
-    me.health_end warrior.health
+    #me.health = warrior.health
+    @health = warrior.health
 
+  end
+
+  def under_attack(current_health)
+        if current_health.to_i < @health.to_i
+            result = true
+        else
+            result = false
+        end
+  end
+
+  def full_hp (current_health)
+      if current_health.to_i >= @max_hp.to_i
+          result = true
+      else
+          result = false
+      end
   end
 
 end
--- src/unit2/rubywarrior/eddie-beginner/warriorstats.rb
+++ src/unit2/rubywarrior/eddie-beginner/warriorstats.rb
@@ -1,32 +1,26 @@
 class WarriorStats
+    @max_hp = 20
+    attr_accessor :health
+    @@health = @max_hp
+
     def initialize
         @max_hp = 20
         @heal = 0.10
-        @@health_begin = @max_hp
-        @@health_end = @max_hp
-        @@under_attack = false
     end
 
-    def health_begin (health)
-        @@health_begin = health
-    end
-    def health_end (health)
-        @@health_end = health
-    end
-
-    def full_hp
-        if @@health_begin >= @max_hp
+    def full_hp ( current_health )
+        if current_health >= @max_hp
             result = true
         else
             result = false
         end
     end
 
-    def under_attack
-        if @@health_end - @@health_begin > 0
-            under_attack = true
+    def under_attack ( current_health )
+        if current_health <= @@health
+            result = true
         else
-            under_attack = false
+            result = false
         end
     end
 end
