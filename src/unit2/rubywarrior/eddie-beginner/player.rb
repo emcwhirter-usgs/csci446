7d9efe8210a81178cea06157ccd7563b3b6d5fbc
Eddie McWhirter <emcwhirter@usgs.gov>
2014-01-18 20:50:00 -0700

    Third attempt at level 4

--- src/unit2/rubywarrior/eddie-beginner/player.rb
+++ src/unit2/rubywarrior/eddie-beginner/player.rb
@@ -13,31 +13,23 @@ class Player
     #thick_sludge_max_hp = 20
     #archer_max_hp = 7
 
-    #me = WarriorStats.new
     space_clear = warrior.feel.empty?
 
-    #if under_attack(warrior.health)
-    if warrior.health < @@health # under attack
-      if warrior.health >= (1-8*@@heal)*@@max_hp and not space_clear # > 20% hp
-        warrior.attack!
-      else
-        warrior.rest!
-      end
-    elsif space_clear
-        #if not me.under_attack(warrior.health) and not me.full_hp(warrior.health)
-        #if not under_attack(warrior.health) and not full_hp(warrior.health)
-        if warrior.health < @@health and warrior.health < @@max_hp
-            warrior.rest!
-        else
-            warrior.walk!
+    if space_clear # Space I want to move to is empty
+      if warrior.health < @@max_hp # I'm hurt
+        if warrior.health >= @@health # not under attack
+          warrior.rest!
+        else # I'm under attack
+          warrior.walk!
         end
-    else
-        warrior.attack!
+      else # I'm full health
+        warrior.walk!
+      end
+    else # Space I want to move to is occupied
+      warrior.attack!
     end
 
-    #me.health = warrior.health
     @health = warrior.health
-
   end
 
   def under_attack(current_health)
