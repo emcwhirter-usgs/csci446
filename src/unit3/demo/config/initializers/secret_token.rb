dfa94229949f04ee293ad9c5ee5d067bf5358d06
Eddie McWhirter <emcwhirter@usgs.gov>
2014-01-25 14:42:43 -0700

    Completed demo from book, chapter 2

---
+++ src/unit3/demo/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Demo::Application.config.secret_key_base = '83fd0d16363b8a2ea6c698f23375b88453ca5f9fecf976c392aa52f9c37ab3c3128631b54da56ad4384a0d43fe88877050b26465a8642593abd5487196785b1d'
