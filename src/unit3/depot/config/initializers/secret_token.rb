fa72e912824bea0ffa5bd07631d8bd04d4a33b31
Eddie McWhirter <emcwhirter@usgs.gov>
2014-01-25 20:58:48 -0700

    Chapter 6 demo seed file working

---
+++ src/unit3/depot/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = 'f88b92da8864711077f159719a648756e69684a625a40f76ea9fb787e150d66080b3c86d4d3875f045a6580f0c8c0a9d8bf46660c745495bc36a22c57ad91f59'
