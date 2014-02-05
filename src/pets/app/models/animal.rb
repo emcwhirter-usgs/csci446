0814ad1715c69696f9a35cecb920992be597fdf9
Eddie McWhirter <emcwhirter@usgs.gov>
2014-02-04 22:01:29 -0700

    Added some validation

--- src/pets/app/models/animal.rb
+++ src/pets/app/models/animal.rb
@@ -1,2 +1,6 @@
 class Animal < ActiveRecord::Base
+  validates :name, :type_of_animal, :description, :presence => true
+
+  validates :age, :numericality => { :greater_than => 0, :less_than_or_equal_to => 500 }
+
 end
