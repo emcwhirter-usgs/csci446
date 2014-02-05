efaf23c3fdf85b809d5700a5ee24a9fc800eecbd
Eddie McWhirter <emcwhirter@usgs.gov>
2014-02-04 22:35:56 -0700

    Added more tests

--- src/pets/test/controllers/animals_controller_test.rb
+++ src/pets/test/controllers/animals_controller_test.rb
@@ -3,6 +3,12 @@ require 'test_helper'
 class AnimalsControllerTest < ActionController::TestCase
   setup do
     @animal = animals(:one)
+    @update = {
+      :name           => 'Mighty',
+      :age            => '1',
+      :type_of_animal => 'Mouse',
+      :description    => 'Here I come to save the day!'
+    }
   end
 
   test "should get index" do
@@ -18,7 +24,8 @@ class AnimalsControllerTest < ActionController::TestCase
 
   test "should create animal" do
     assert_difference('Animal.count') do
-      post :create, animal: { age: @animal.age, description: @animal.description, name: @animal.name, type_of_animal: @animal.type_of_animal }
+      #post :create, animal: { age: @animal.age, description: @animal.description, name: @animal.name, type_of_animal: @animal.type_of_animal }
+      post :create, animal: @update
     end
 
     assert_redirected_to animal_path(assigns(:animal))
@@ -35,7 +42,8 @@ class AnimalsControllerTest < ActionController::TestCase
   end
 
   test "should update animal" do
-    patch :update, id: @animal, animal: { age: @animal.age, description: @animal.description, name: @animal.name, type_of_animal: @animal.type_of_animal }
+    #patch :update, id: @animal, animal: { age: @animal.age, description: @animal.description, name: @animal.name, type_of_animal: @animal.type_of_animal }
+    put :update :id => @animal.to_param, :animal => @update
     assert_redirected_to animal_path(assigns(:animal))
   end
 
--- src/pets/test/models/animal_test.rb
+++ src/pets/test/models/animal_test.rb
@@ -4,4 +4,35 @@ class AnimalTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "animal attributes must not be empty" do
+    animal = Animal.new
+    assert animal.invalid?
+    assert animal.errors[:name].any?
+    assert animal.errors[:age].any?
+    assert animal.errors[:type_of_animal].any?
+    assert animal.errors[:description].any?
+  end
+
+  test "animal age must be reasonable" do
+    animal = Animal.new(name:       "My Friend",
+                        type_of_animal: "Elephant",
+                        description:   "Large & in charge")
+    animal.age = -1
+    assert animal.invalid?
+    assert_equal ["can't be negative"],
+      animal.errors[:animal]
+
+    animal.age = 0
+    assert animal.invalid?
+    assert_equal ["must be greater than 0"],
+      animal.errors[:animal]
+
+    animal.age = 9000
+    assert animal.invalid?
+    assert_equal ["must be less than or equal to 500"],
+      animal.errors[:animal]
+
+    animal.age = 1
+    assert product.valid?
+  end
 end
